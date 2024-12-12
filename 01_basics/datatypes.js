61fddf8d5e98f7e7e6ee4838e0cab830622ca072
khushi02410 <khushirajpurohit2211@gmail.com>
2024-12-12 14:32:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newwer version/js
+//alert(3 * 3 ) // we are using node js not browser it will not support 
+
+//datatypes 
+//num => 2 to power 43
+//bigint 
+//string => " "
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol  => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null) ; //object in the output will be shown
