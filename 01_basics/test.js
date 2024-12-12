1f3beda873819d25eba264959af1f49df6a8a42e
khushi02410 <khushirajpurohit2211@gmail.com>
2024-12-12 14:33:44 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("kkr")
+
+//console.log("kkr")
+    
+const accid = "547696" 
+let name = "khushi"  
+var accnumber = "8766789"
+age = "43"
+//accid = "34566543"
+console.log(accid)
+
+/*
+prefer not to use var becaue of aissue in block scope and functional scope
+*/
+
+console.table([accid, name , accnumber, age])
+
