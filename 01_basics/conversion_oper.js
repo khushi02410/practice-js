ecfcda02c68dcd749c219af029a75c239881dfe9
khushi02410 <khushirajpurohit2211@gmail.com>
2024-12-17 18:20:44 +0000

    conversion_oper.js

---
+++ 01_basics/conversion_oper.js
@@ -0,0 +1,25 @@
+/*let a = "33";
+let convert = Number(a);
+console.log(convert);
+console.log(typeof a);
+*/
+
+/*let b = undefined
+console.log(typeof b);
+let b1 = Number(b)
+console.log(b1)
+*/
+
+/*let c = null
+console.log(typeof c)
+let convert = Number(c)
+console.log(convert)
+//output will be obj and 0 when cannot be converted to no aur required*/
+
+let d = "mitesh"
+console.log(typeof d)
+let convert = Number(d)
+console.log(convert)
+// output will be string and NaN because cannot convert string into number
+
+  
